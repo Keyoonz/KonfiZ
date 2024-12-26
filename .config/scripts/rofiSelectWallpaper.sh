f9c90e0153fc22177a648250f10e1060be3e24a4
Keyoonz <bonnardmae@gmail.com>
2024-12-26 14:15:11 +0100

    new script for wallpaper with images preview

--- .config/scripts/rofiSelectWallpaper.sh
+++ .config/scripts/rofiSelectWallpaper.sh
@@ -1,4 +1,6 @@
-availablewallpaper=$(ls ~/.config/assets/wallpaper/)
-availablewallpaper=$(sed "s_ _\n_g" <<<$availablewallpaper)
-chosenwallpaper=$(echo $availablewallpaper | sed "s_ _\n_g" | rofi -dmenu)
-~/.config/scripts/set-wallpaper.sh ~/.config/assets/wallpaper/$chosenwallpaper
+shopt -s extglob
+chosenwallpaper=$(for a in ~/.config/assets/wallpaper/?(*.jpg|*.png); do echo -en "$(sed "s_.*wallpaper/\(.*\)-wallpaper.\w*_\1_" <<<$a)\0icon\x1f$a\n"; done | rofi -dmenu -p "Select Wallpaper")
+if [[ ! -z $chosenwallpaper ]]; then
+  chosenwallpaperpath=$(ls ~/.config/assets/wallpaper/$chosenwallpaper-wallpaper.*)
+  ~/.config/scripts/set-wallpaper.sh $chosenwallpaperpath
+fi
