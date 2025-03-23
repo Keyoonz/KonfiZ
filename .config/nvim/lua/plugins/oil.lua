6efbe49dcfa4525514823ec9e85635d40d03226d
Keyoonz <bonnardmae@gmail.com>
2025-03-23 13:28:39 +0100

    special workspace scale factor change

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    init = function()
+      require("oil").setup()
+
+      vim.keymap.set("n", "<leader>dd", "<CMD>Oil<CR>", { desc = "Open directory as buffer" })
+    end,
+  },
+}
