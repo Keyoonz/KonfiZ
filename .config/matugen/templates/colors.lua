330b3c2cd6128462a9856722b467583b46644888
Keyoonz <bonnardmae@gmail.com>
2025-11-18 17:30:27 +0100

    adding nvim-kolorz changing with wallpaper

---
+++ .config/matugen/templates/colors.lua
@@ -0,0 +1,24 @@
+return {
+	-- Backgrounds & foregrounds
+	c_normal_fg = "#f2b3e5",
+	c_normal_bg = "#171216",
+
+	-- Syntax groups
+	c_comment = "#4d1f48",
+	c_string = "#f5b8a6",
+	c_identifier = "#f2b3e5",
+	c_function = "#dbbed2",
+	c_statement = "#f2b3e5",
+	c_keyword = "#f2b3e5",
+	c_type = "#f2b3e5",
+	c_error = "#ffb4ab",
+	c_visual = "#f2b3e5",
+
+	-- Line numbers
+	c_line_nr = "#4d1f48",
+	c_cursor_line_nr = "#f2b3e5",
+
+	-- Statusline
+	c_status_line_fg = "#f2b3e5",
+	c_status_line_bg = "#171216",
+}
